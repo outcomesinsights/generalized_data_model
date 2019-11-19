bb9265590fe60571efd6587304ce35c97fa91ebf
Ryan Duryea <aguynamedryan@gmail.com>
2019-11-19 21:02:21 +0000

    schema: add some indexes

--- convert_to_schema.rb
+++ convert_to_schema.rb
@@ -1,4 +1,4 @@
-require 'pathname'
+require "pathname"
 
 table = nil
 collect = false
@@ -100,7 +100,66 @@ def convert(name, type)
 end
 
 schema_io = SequelMigrationIO.new
-fks = {}
+indexes = {
+  patients: [
+    :gender_concept_id,
+    :race_concept_id,
+    :ethnicity_concept_id,
+  ],
+  patient_details: [
+    :patient_id
+  ],
+  practictioners: [
+    :specialty_concept_id
+  ],
+  collections: [
+    :patient_id
+  ],
+  contexts_practitioners: [
+    :specialty_concept_id
+  ],
+  contexts: [
+    :collection_id,
+    :source_type_concept_id
+  ],
+  clinical_codes: [
+    :patient_id,
+    :clinical_code_concept_id
+  ],
+  costs: [
+    :patient_id
+  ],
+  deaths: [
+    :patient_id
+  ],
+  information_periods: [
+    :patient_id,
+    :information_type_concept_id
+  ],
+  concepts: [
+    [ :vocabulary_id, "Sequel.function(:lower, :concept_code)" ],
+    [ :vocabulary_id, :concept_code ]
+  ],
+  mappings: [
+    :concept_1_id
+  ]
+}
+
+table = nil
+
+def apply_indexes(io, table, indexes)
+  (indexes || []).each do |index|
+    columns_str = "[ " + Array(index).map do |col|
+      case col
+      when Symbol
+        col.inspect
+      else
+        col
+      end
+    end.join(", ") + " ]"
+    io.puts "add_index #{table.inspect}, #{columns_str}"
+  end
+end
 
 File.foreach('README.md') do |line|
   line.chomp!
@@ -114,6 +173,7 @@ File.foreach('README.md') do |line|
     next
   when ''
     if collect
+      apply_indexes(schema_io, table, indexes[table])
       schema_io.close_block
     end
     collect = false
@@ -130,14 +190,14 @@ File.foreach('README.md') do |line|
     column_opts.merge!(comment: comment)
     column_opts.merge!(null: false) if required && !required.strip.empty?
     if foreign_key
-      (fks[table] ||= {})[name] = foreign_key
+      schema_io.puts "foreign_key #{name.inspect}, #{foreign_key.to_sym.inspect}, #{column_opts.merge(type: "Bigint".to_sym, key: :id).inspect}"
+    else
+      schema_io.puts "#{type} #{name.inspect}#{column_opts.empty? ? '' : ", #{column_opts.inspect}"}"
     end
-    schema_io.puts "#{type} #{name.inspect}#{column_opts.empty? ? '' : ", #{column_opts.inspect}"}"
   end
 end
 
+apply_indexes(schema_io, table, indexes[table])
 schema_io.finish
 
-
 File.write(artifacts_dir + "schema.rb", schema_io.string)
-#File.write(artifacts_dir + "foreign_keys.rb", fk_io.string)
